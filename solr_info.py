15ff64e8259f3391bcb6b2343e9d913b8dcf4ab6
Jernej Virag <jernej@virag.si>
2013-10-31 15:22:55 +0100

    Implemented proper data dispatch

--- solr_info.py
+++ solr_info.py
@@ -3,8 +3,20 @@ import urllib2
 import json
 import itertools
 
-SOLR_CORE_URL = "http://localhost:8983/solr/news"
+SOLR_CORE_URL = "http://localhost:8080/solr/news"
 SOLR_HANDLERS = {"query": "/select", "suggest": "/suggest", "simillar": "/mlt"}
+
+def dispatch_value(value, value_name, value_type, type_instance=None):
+    val = collectd.Values(plugin="solr_info")
+    val.type = value_type
+
+    if type_instance is not None:
+       val.plugin_instance = value_name
+       val.type_instance = type_instance
+    else:
+       val.type_instance = value_name
+    val.values = [value]
+    val.dispatch()
 
 def fetch_data():
     global SOLR_CORE_URL, SOLR_HANDLERS
@@ -54,7 +66,30 @@ def fetch_data():
             data["update_stats"]["errors"] = module_data["updateHandler"]["stats"]["errors"]
     return data
 
-# Used only for testing, remove when done!
-if __name__ == "__main__":
-   data = fetch_data() 
-   print data
+def read_callback():
+    data = fetch_data()
+    dispatch_value(data["docs"], "index", "gauge", "documents")
+    dispatch_value(data["cache"]["size"], "cache", "gauge", "size")
+    dispatch_value(data["cache"]["hitratio"], "cache_hitratio", "gauge", "hitratio")
+    dispatch_value(data["cache"]["evictions"], "cache", "gauge", "evictions")
+
+    for handler_name, handler_data in data["handler_stats"].iteritems():
+        dispatch_value(handler_data["requests"], handler_name, "gauge", "requests")
+        dispatch_value(handler_data["errors"], handler_name, "gauge", "errors")
+        dispatch_value(handler_data["timeouts"], handler_name, "gauge", "timeouts")
+        dispatch_value(handler_data["time_per_request"], "request_times", "gauge", handler_name)
+        dispatch_value(handler_data["requests_per_second"], "requests_per_second", "gauge", handler_name)
+
+    dispatch_value(data["update_stats"]["commits"], "update", "gauge", "commits")
+    dispatch_value(data["update_stats"]["autocommits"], "update", "gauge", "autocommits")
+    dispatch_value(data["update_stats"]["soft_autocommits"], "update", "gauge", "soft_autocommits")
+    dispatch_value(data["update_stats"]["optimizes"], "update", "gauge", "optimizes")
+    dispatch_value(data["update_stats"]["expunges"], "update", "gauge", "expunges")
+    dispatch_value(data["update_stats"]["rollbacks"], "update", "gauge", "rollbacks")
+    dispatch_value(data["update_stats"]["pending_docs"], "update", "gauge", "pending_docs")
+    dispatch_value(data["update_stats"]["adds"], "update", "gauge", "adds")
+    dispatch_value(data["update_stats"]["deletes_by_id"], "update", "gauge", "deletes_by_id")
+    dispatch_value(data["update_stats"]["deletes_by_query"], "update", "gauge", "deletes_by_query")
+    dispatch_value(data["update_stats"]["errors"], "update", "gauge", "errors")
+
+collectd.register_read(read_callback)
